2cf81f7fbd1c1d9a66e4794e0a26112aa22bdb34
Griffin Garland <griffin.garland@gmail.com>
2021-12-05 19:07:45 -0500

    Done first challenge

---
+++ aoc1/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+fn main() {
+	// Hardcoded so nobody can steal my answer >:(
+    let filename = "/home/griffin/src/aoc/aoc1/input";
+
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+                .expect("Something went wrong reading the file");
+
+	let split = contents.trim().split("\n");
+
+	let mut is_first_value = true;
+	let mut old_value = 0;
+	let mut number_of_increases = 0;
+
+	for value in split {
+		match is_first_value {
+			true => is_first_value = false,
+			false => {
+				let value_parsed: u32 = value.trim().parse().expect("Please type a number!");
+				if value_parsed > old_value {
+					number_of_increases += 1;
+				}
+				old_value = value_parsed;
+			}
+		};
+	}
+	println!("{}", number_of_increases);
+
+}
